724b57a4c6a9ce71b1cea7106690f0f6c3d310b7
Mamo-00 <mahmodahmed2000@gmail.com>
2022-10-13 16:25:50 +0200

    added modified input radio group component added a blogpost creation model for validation modified some css

---
+++ CreativeBlogs/Models/CreateBlogPostModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CreativeBlogs.Models;
+
+public class CreateBlogPostModel
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(150)]
+    public string Title { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    [Display(Name = "Tag")]
+    public string TagId { get; set; }
+
+    [MaxLength(32000)]
+    public string Description { get; set; }
+}
--- CreativeBlogsLibrary/DataAccess/MongoTagsData.cs
+++ CreativeBlogsLibrary/DataAccess/MongoTagsData.cs
@@ -4,28 +4,31 @@ namespace CreativeBlogsLibrary.DataAccess;
 public class MongoTagsData : ITagsData
 {
 	private readonly IMongoCollection<TagModel> tags;
+	private readonly IMongoCollection<BlogPostModel> blogposts;
 	private readonly IMemoryCache cache;
 	private const string CacheName = "TagData";
 	public MongoTagsData(IDbConnection db, IMemoryCache cache)
 	{
 		this.cache = cache;
 		this.tags = db.TagCollection;
+		blogposts = db.BlogPostCollection;
 	}
 
 	public async Task<List<TagModel>> GetAllTags()
 	{
-		var output = this.cache.Get<List<TagModel>>(CacheName);
+		var output = cache.Get<List<TagModel>>(CacheName);
 		if (output is null)
 		{
 			var results = await this.tags.FindAsync(_ => true);
 			output = results.ToList();
 
-			this.cache.Set(CacheName, output, TimeSpan.FromDays(1));
+			cache.Set(CacheName, output, TimeSpan.FromDays(1));
 		}
 		return output;
 	}
 
-	public Task CreateTags(TagModel tags)
+    
+    public Task CreateTags(TagModel tags)
 	{
 		return this.tags.InsertOneAsync(tags);
 	}
